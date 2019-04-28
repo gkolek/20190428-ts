70da9226bbe635e07d275f30100957cec0e900cd
Grzegorz Kolek <gkolek@gazeta.pl>
2019-04-28 09:57:49 +0200

    ex2.2

--- exercise2.ts
+++ exercise2.ts
@@ -3,10 +3,18 @@ enum Role {
     Admin,
     User
 }
-var users = [
+interface User {
+    id: number;
+    userName: string;
+    role: Role;
+    hairColor: string
+}
+
+var users:Array<User> = [
     { id: 1, userName: 'first', role: Role.Admin },
     { id: 2, userName: 'seconds', role: Role.SuperAdmin },
     { id: 3, userName: 'third', role: Role.User },
+    { id: 4, userName: 'forth', role: Role.User, hairColor:'blond' },
 ]
 
 
